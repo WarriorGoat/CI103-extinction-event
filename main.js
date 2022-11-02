665fce0194f772ba4fa91f9c123cc48eb16b6634
WarriorGoat <michael.d.spitz@gmail.com>
2022-11-02 14:27:52 -0400

    Minor Updates

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 let list1 = document.querySelectorAll("ol li");
 for(let i = 0; i< list1.length; i++){
     list1[i].addEventListener("click", function(){
-        strike(list1[i])})}
+        strike(list1[i]);
+    })}
 
 let list2 = document.querySelectorAll("ul li");
 for(let i = 0; i< list2.length; i++){
     list2[i].addEventListener("click", function(){
-        fade(list2[i])})}
+        fade(list2[i]);
+    })}
 
 let picture = document.querySelectorAll("img");
 for(let i = 0; i< picture.length; i++){
     picture[i].addEventListener("click", function(){
-        collapse (picture[i])})}
+        collapse (picture[i]);
+    })}
 
 let meteor = document.querySelector ("button");
 meteor.addEventListener("click", function(){
@@ -21,8 +24,8 @@ meteor.addEventListener("click", function(){
 })
 
 //FUNCTION LIBRARY
-function strike(item){item.style.textDecoration = "line-through"}
+function strike(item){item.style.textDecoration = "line-through"};
 
-function fade(item){item.style.opacity = 0}
+function fade(item){item.style.opacity = 0};
 
-function collapse(item){item.style.width = 0}
+function collapse(item){item.style.width = 0};
